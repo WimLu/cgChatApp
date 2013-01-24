e71ca44985b6707aa39371d768a96d3fac28b1a2
Wim <wim@MacBook-Pro-van-Wim.local>
2013-01-24 16:05:42 +0100

    Initial Commit

---
+++ cgChatApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  cgChatApp
+//
+//  Created by Wim on 24/01/13.
+//  Copyright (c) 2013 Wim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ cgChatApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  cgChatApp
+//
+//  Created by Wim on 24/01/13.
+//  Copyright (c) 2013 Wim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
